152649450756627abeeef838a79ed14042c38e30
Marcio Serafim <marciofilho2@gmail.com>
2022-03-16 13:21:26 -0300

    Adicionado classe dto para o veiculo

---
+++ src/main/java/com/tinnova/desafio/ex5/dto/VeiculoDto.java
@@ -0,0 +1,29 @@
+package com.tinnova.desafio.ex5.dto;
+
+import com.tinnova.desafio.ex5.model.Veiculo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VeiculoDto {
+
+    private long id;
+    private String veiculo;
+    private String marca;
+    private int ano;
+    private String descricao;
+    private boolean vendido;
+    private Date created;
+    private Date updated;
+
+    public VeiculoDto(Veiculo veiculo){
+        BeanUtils.copyProperties(veiculo, this);
+    }
+
+}
