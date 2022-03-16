e50c13111f8de4189f062d3a1b2e4d081c6a8bfa
Marcio Serafim <marciofilho2@gmail.com>
2022-03-16 13:22:06 -0300

    Adicionado interface de repository ara o veiculo

---
+++ src/main/java/com/tinnova/desafio/ex5/repository/VeiculoRepository.java
@@ -0,0 +1,7 @@
+package com.tinnova.desafio.ex5.repository;
+
+import com.tinnova.desafio.ex5.model.Veiculo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VeiculoRepository extends CrudRepository<Veiculo, Long> {
+}
