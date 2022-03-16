e7f783cdac0d62e2e7fd5428d1ea076267683c6a
Marcio Serafim <marciofilho2@gmail.com>
2022-03-16 15:37:39 -0300

    Arrumando alguns parametros no controller e a implementação da atualização parcial

--- src/main/java/com/tinnova/desafio/ex5/service/impl/VeiculoServiceImpl.java
+++ src/main/java/com/tinnova/desafio/ex5/service/impl/VeiculoServiceImpl.java
@@ -6,11 +6,11 @@ import com.tinnova.desafio.ex5.model.Veiculo;
 import com.tinnova.desafio.ex5.repository.VeiculoRepository;
 import com.tinnova.desafio.ex5.service.VeiculoService;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -65,17 +65,20 @@ public class VeiculoServiceImpl implements VeiculoService {
         }
     }
 
-    // TODO: 16/03/2022 - Melhorar para atualizar apenas os campos preenchidos
     public VeiculoDto atualizaParcialVeiculoPorId(VeiculoDto veiculoDto, long id){
-        Optional<Veiculo> veiculo = veiculoRepository.findById(id);
+        Optional<Veiculo> veiculoOptional = veiculoRepository.findById(id);
 
-        if (veiculo.isPresent()) {
-            Date created = veiculo.get().getCreated();
-            BeanUtils.copyProperties(veiculoDto, veiculo.get());
-            veiculo.get().setCreated(created);
-            veiculo.get().setUpdated(new Date());
-            veiculo.get().setId(id);
-            return new VeiculoDto(veiculoRepository.save(veiculo.get()));
+        if (veiculoOptional.isPresent()) {
+            Veiculo veiculo = veiculoOptional.get();
+
+            veiculo.setVeiculo(Strings.isBlank(veiculoDto.getVeiculo()) ? veiculo.getVeiculo() : veiculoDto.getVeiculo());
+            veiculo.setAno(veiculoDto.getAno() == 0 ? veiculo.getAno() : veiculoDto.getAno());
+            veiculo.setDescricao(Strings.isBlank(veiculoDto.getDescricao()) ? veiculo.getDescricao() : veiculoDto.getDescricao());
+            veiculo.setVendido(veiculoDto.isVendido());
+            veiculo.setMarca(Strings.isBlank(veiculoDto.getMarca()) ? veiculo.getMarca() : veiculoDto.getMarca());
+            veiculo.setUpdated(new Date());
+
+            return new VeiculoDto(veiculoRepository.save(veiculo));
 
         } else {
             throw new VeiculoNotFoundException("Não foi possível encontrar o veiculo de id " + id);
